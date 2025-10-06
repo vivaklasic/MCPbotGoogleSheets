354a64c16daeda1708964a57c2d2ac51ed98643d
vivaklasic <vivaklasic@gmail.com>
2025-10-06 23:19:01 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -8,13 +8,12 @@ app.use(express.json());
 // === Google Sheets Setup ===
 const auth = new google.auth.GoogleAuth({
   credentials: {
-    type: "service_account",
-    project_id: process.env.GOOGLE_PROJECT_ID,
-    private_key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
     client_email: process.env.GOOGLE_CLIENT_EMAIL,
+    private_key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, "\n")
   },
-  scopes: ["https://www.googleapis.com/auth/spreadsheets.readonly"],
+  scopes: ["https://www.googleapis.com/auth/spreadsheets.readonly"]
 });
+
 
 const sheets = google.sheets({ version: "v4", auth });
 
